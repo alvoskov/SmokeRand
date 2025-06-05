13057ea4bd72a92b1f04984326e09ecd7519e888
Alexey Voskov <alvoskov@gmail.com>
2025-06-05 20:03:01 +0300

    MWC research

--- generators/mwc192.c
+++ generators/mwc192.c
@@ -35,21 +35,27 @@ typedef struct {
 
 /**
  * @brief MWC128 PRNG implementation.
+ * @details Optimization of the output function (made for the very small bad
+ * multiplier 0x621):
+ *
+ * 1. Optimize the shift in the x_new ^ (x_2 <<< sh1) output.
+ * 2. Test if for the (Q x_new) ^ (x_2 <<< sh1) output.
  */
 static inline uint64_t get_bits_raw(void *state)
 {
 //    static const uint64_t MWC_A1 = 0xf21f494c589bf715;
+//    static const uint64_t MWC_A1 = 0x8F1;
     static const uint64_t MWC_A1 = 0x621;
+
+    static const uint64_t LCG_A1 = 18000690696906969069ULL;
     MWC192State *obj = state;
-//    uint64_t u = (obj->x ^ rotl64(obj->x2, 5)) + rotl64(obj->x, 51);
+    uint64_t out = (LCG_A1 * obj->x) ^ rotl64(obj->x2, 11); // >= 2 TiB
+    //uint64_t out = (obj->x) ^ rotl64(obj->x2, 11); // 32 GiB (without scramblers - 16 MiB)
     uint64_t x_new = unsigned_muladd128(MWC_A1, obj->x2, obj->c, &obj->c);
     obj->x2 = obj->x;
     obj->x = x_new;
-//    return u;
-    return (x_new ^ rotl64(obj->x2, 5)) + rotl64(x_new, 51);
+    return out;
 }
-
-
 
 static void *create(const CallerAPI *intf)
 {
--- misc/mwc_ntap.py
+++ misc/mwc_ntap.py
@@ -2,10 +2,10 @@
 from sympy import *
 
 b = 2**64
-#dmin = 10**18 ##2**64 - 100000
-dmin = 2**64 - 100000
+#dmin, dmin_inc = 2**64 - 100000, 99999
+dmin, dmin_inc = 5**26, 1000000
 
-for i in range(dmin, dmin + 10000000):
+for i in range(dmin, dmin + dmin_inc):
     a = (b - i)
     m = a*b**2 - 1
     if isprime(m) and isprime((m - 1) // 2):
