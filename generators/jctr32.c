5b419125a9c0f80cb10a9c7295340beda51e5c10
Восков Алексей <alvoskov@gmail.com>
2025-09-30 09:52:37 +0300

    jctr code cleanup

--- generators/jctr32.c
+++ generators/jctr32.c
@@ -1,27 +1,64 @@
-// 
-// https://burtleburtle.net/bob/c/myblock.c
+/**
+ * @brief jctr32 is a counter-based PRNG based on an experimental block
+ * cipher by Bob Jenkins.
+ * @details The original cipher is a 256-bit block ARX cipher based on 32-bit
+ * arithmetics. The author didn't made a formal cryptoanalysis and positioned
+ * it as "guaranteed to be stronger than rot-13".
+ * 
+ * Modifications made by A.L. Voskov:
+ *
+ * 1. Number of rounds was reduced to 6 (even 4 round pass SmokeRand `full`
+ *    battery, and it is not for crypto anyway).
+ * 2. It is converted to the construction similar to the stream cipher
+ *    like ChaCha20.
+ *
+ * WARNING! NOT FOR CRYPTOGRAPHY! Use only as a general purpose CBPRNG!
+ *
+ * Refences:
+ *
+ * 1. https://burtleburtle.net/bob/crypto/myblock.html
+ * 2. https://burtleburtle.net/bob/c/myblock.c
+ * 3. https://oeis.org/A062964
+ * 4. https://gist.github.com/retrohacker/e5fff72b7b75ee058924
+ *
+ * @copyright The original 256-bit ARX block cipher was designed
+ * by Bob Jenkins and released as Public Domain.
+ *
+ * Conversion to `jctr32` counter-based PRNG:
+ * 
+ * (c) 2025 Alexey L. Voskov, Lomonosov Moscow State University.
+ * alvoskov@gmail.com
+ *
+ * This software is licensed under the MIT license.
+ */
 #include "smokerand/cinterface.h"
 
 PRNG_CMODULE_PROLOG
 
-
+/**
+ * @brief Jctr32 counter-based PRNG state.
+ * @details The state has the next layout:
+ *
+ *     | pi     key key ctr_lo |
+ *     | ctr_hi pi  key key    |
+ */
 typedef struct {
-    uint32_t x[8];
-    uint32_t out[8];
+    uint32_t x[8];   ///< Working state
+    uint32_t out[8]; ///< Output state
     size_t pos;
 } Jctr32State;
 
 
 static inline void jctr32_round(uint32_t *x)
 {
-   x[0] -= x[4]; x[5] ^= x[7] >> 8;  x[7] += x[0];
-   x[1] -= x[5]; x[6] ^= x[0] << 8;  x[0] += x[1];
-   x[2] -= x[6]; x[7] ^= x[1] >> 11; x[1] += x[2];
-   x[3] -= x[7]; x[0] ^= x[2] << 3;  x[2] += x[3];
-   x[4] -= x[0]; x[1] ^= x[3] >> 6;  x[3] += x[4];
-   x[5] -= x[1]; x[2] ^= x[4] << 4;  x[4] += x[5];
-   x[6] -= x[2]; x[3] ^= x[5] >> 13; x[5] += x[6];
-   x[7] -= x[3]; x[4] ^= x[6] << 13; x[6] += x[7];
+    x[0] -= x[4]; x[5] ^= x[7] >> 8;  x[7] += x[0];
+    x[1] -= x[5]; x[6] ^= x[0] << 8;  x[0] += x[1];
+    x[2] -= x[6]; x[7] ^= x[1] >> 11; x[1] += x[2];
+    x[3] -= x[7]; x[0] ^= x[2] << 3;  x[2] += x[3];
+    x[4] -= x[0]; x[1] ^= x[3] >> 6;  x[3] += x[4];
+    x[5] -= x[1]; x[2] ^= x[4] << 4;  x[4] += x[5];
+    x[6] -= x[2]; x[3] ^= x[5] >> 13; x[5] += x[6];
+    x[7] -= x[3]; x[4] ^= x[6] << 13; x[6] += x[7];
 }
 
 void Jctr32State_block(Jctr32State *obj)
@@ -56,10 +93,12 @@ void Jctr32State_init(Jctr32State *obj, const uint64_t *key, uint64_t ctr)
     Jctr32State_block(obj);
 }
 
+
 static inline void Jctr32State_inc_counter(Jctr32State *obj)
 {
     if (obj->x[3]++) obj->x[4]++;
 }
+
 
 static inline uint64_t get_bits_raw(void *state)
 {
--- generators/jctr64.c
+++ generators/jctr64.c
@@ -1,27 +1,69 @@
-// 
-// https://burtleburtle.net/bob/c/myblock.c
+/**
+ * @brief jctr64 is a counter-based PRNG based on an experimental block
+ * cipher by Bob Jenkins.
+ * @details The original cipher is a 512-bit block ARX cipher based on 64-bit
+ * arithmetics. The author didn't made a formal cryptoanalysis and positioned
+ * it as "use this only if you are a fool". It is also used as during the
+ * official ISAAC64 initialization procedure.
+ * 
+ * Modifications made by A.L. Voskov:
+ *
+ * 1. Number of rounds was reduced to 6 (even 4 round pass SmokeRand `full`
+ *    battery, and it is not for crypto anyway).
+ * 2. It is converted to the construction similar to the stream cipher
+ *    like ChaCha20.
+ *
+ * WARNING! NOT FOR CRYPTOGRAPHY! Use only as a general purpose CBPRNG!
+ *
+ * Refences:
+ *
+ * 1. https://burtleburtle.net/bob/crypto/myblock.html
+ * 2. https://burtleburtle.net/bob/c/myblock.c
+ * 3. https://oeis.org/A062964
+ * 4. https://gist.github.com/retrohacker/e5fff72b7b75ee058924
+ *
+ * @copyright The original 512-bit ARX block cipher was designed
+ * by Bob Jenkins and released as Public Domain.
+ *
+ * Conversion to `jctr64` counter-based PRNG:
+ * 
+ * (c) 2025 Alexey L. Voskov, Lomonosov Moscow State University.
+ * alvoskov@gmail.com
+ *
+ * This software is licensed under the MIT license.
+ */
 #include "smokerand/cinterface.h"
 
 PRNG_CMODULE_PROLOG
 
-
+/**
+ * @brief Jctr64 counter-based PRNG state.
+ * @details The state has the next layout:
+ *
+ *     | pi  key  pi  ctr_lo |
+ *     | pi  key  pi  ctr_hi |
+ */
 typedef struct {
-    uint64_t x[8];
-    uint64_t out[8];
+    uint64_t x[8];   ///< Working state
+    uint64_t out[8]; ///< Output state
     size_t pos;
 } Jctr64State;
 
+/**
+ * @brief Taken from ISAAC64 source code equipped with test vectors.
+ */
 static inline void jctr64_round(uint64_t *x)
 {
-   x[0] -= x[4]; x[5] ^= x[7] >> 9;  x[7] += x[0];
-   x[1] -= x[5]; x[6] ^= x[0] << 9;  x[0] += x[1];
-   x[2] -= x[6]; x[7] ^= x[1] >> 23; x[1] += x[2];
-   x[3] -= x[7]; x[0] ^= x[2] << 15; x[2] += x[3];
-   x[4] -= x[0]; x[1] ^= x[3] >> 14; x[3] += x[4];
-   x[5] -= x[1]; x[2] ^= x[4] << 20; x[4] += x[5];
-   x[6] -= x[2]; x[3] ^= x[5] >> 17; x[5] += x[6];
-   x[7] -= x[3]; x[4] ^= x[6] << 14; x[6] += x[7];
+    x[0] -= x[4]; x[5] ^= x[7] >> 9;  x[7] += x[0];
+    x[1] -= x[5]; x[6] ^= x[0] << 9;  x[0] += x[1];
+    x[2] -= x[6]; x[7] ^= x[1] >> 23; x[1] += x[2];
+    x[3] -= x[7]; x[0] ^= x[2] << 15; x[2] += x[3];
+    x[4] -= x[0]; x[1] ^= x[3] >> 14; x[3] += x[4];
+    x[5] -= x[1]; x[2] ^= x[4] << 20; x[4] += x[5];
+    x[6] -= x[2]; x[3] ^= x[5] >> 17; x[5] += x[6];
+    x[7] -= x[3]; x[4] ^= x[6] << 14; x[6] += x[7];
 }
+
 
 void Jctr64State_block(Jctr64State *obj)
 {
@@ -41,6 +83,7 @@ void Jctr64State_block(Jctr64State *obj)
     }
 }
 
+
 void Jctr64State_init(Jctr64State *obj, const uint64_t *key, uint64_t ctr)
 {
     obj->x[0] = 0x243F6A8885A308D3;
@@ -50,15 +93,17 @@ void Jctr64State_init(Jctr64State *obj, const uint64_t *key, uint64_t ctr)
     obj->x[4] = 0xA4093822299F31D0;
     obj->x[5] = key[1];
     obj->x[6] = 0x082EFA98EC4E6C89;
-    obj->x[7] = 0;
+    obj->x[7] = 0; // Reserved for an upper half of a counter
     obj->pos = 0;
     Jctr64State_block(obj);
 }
+
 
 static inline void Jctr64State_inc_counter(Jctr64State *obj)
 {
     obj->x[3]++;
 }
+
 
 static inline uint64_t get_bits_raw(void *state)
 {
