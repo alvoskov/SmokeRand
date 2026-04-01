b22f96afe7eab278180908848f672e1c50065d41
Восков Алексей <alvoskov@gmail.com>
2026-04-02 01:39:58 +0300

    mwcfp improvements

--- generators/mwcfp.c
+++ generators/mwcfp.c
@@ -44,6 +44,20 @@ typedef struct {
 
 
 typedef struct {
+    uint64_t x[17];
+    uint64_t c;
+    int pos;
+} MwcFp1024u64State;
+
+
+typedef struct {
+    uint64_t x[5];
+    uint64_t c;
+    int pos;
+} MwcFp256u64State;
+
+
+typedef struct {
     uint32_t x[9];
     uint32_t c;
     int pos;
@@ -102,19 +116,20 @@ static inline uint64_t funcname(type *obj) \
             obj->x[i] = unsigned_muladd128(a, obj->x[i], c, &c); \
         } \
         obj->c = a * obj->x[(lag)] + c; /* H */ \
-        /* H*2^(kw) + L = H*(2^(kw) + 1) + (L - H) */ \
-        uint64_t b = sub64b(&obj->x[0], obj->c); \
-        for (int i = 1; i < (lag); i++) { \
+        /* H*2^(kw) + L = H*(2^(kw) + 2) + (L - 2*H) */ \
+        uint64_t b = sub64b(&obj->x[0], obj->c << 1); \
+        b = sub64b(&obj->x[1], (obj->c >> 63) + b); \
+        for (int i = 2; i < (lag); i++) { \
             b = sub64b(&obj->x[i], b); \
         } \
-        /* Process a special case: L < H \
-           H*2^(kw) + L = (H - 1)*(2^(kw) + 1) + (2^(kw) + 1 + L - H) \
-           Note that (L - H) mod 2^(kw) gives (L - H + 2^(kw)) */ \
+        /* Process a special case: L < 2*H \
+           H*2^(kw) + L = (H - 1)*(2^(kw) + 2) + (2^(kw) + 2 + L - 2*H) \
+           Note that (L - 2*H) mod 2^(kw) gives (L - H + 2^(kw)) */ \
         if (b == 0) { \
             obj->x[(lag)] = 0; \
         } else { \
             obj->c--; \
-            c = 1; \
+            c = 2; \
             for (int i = 0; i < (lag); i++) { \
                 c = add64c(&obj->x[i], c); \
             } \
@@ -122,7 +137,8 @@ static inline uint64_t funcname(type *obj) \
         } \
         obj->pos = 0; \
     } \
-    return obj->x[obj->pos++]; \
+    const uint64_t u = obj->x[obj->pos++]; \
+    return u; \
 }
 
 
@@ -138,19 +154,20 @@ static inline uint64_t funcname(type *obj) \
             obj->x[i] = (uint32_t) ui; c = (uint32_t) (ui >> 32); \
         } \
         obj->c = (uint32_t) (a * obj->x[(lag)] + c); /* H */ \
-        /* H*2^(kw) + L = H*(2^(kw) + 1) + (L - H) */ \
-        uint32_t b = sub32b(&obj->x[0], obj->c); \
-        for (int i = 1; i < (lag); i++) { \
+        /* H*2^(kw) + L = H*(2^(kw) + 1) + (L - 2H) */ \
+        uint32_t b = sub32b(&obj->x[0], obj->c << 1); \
+        b = sub32b(&obj->x[1], (obj->c >> 31) + b); \
+        for (int i = 2; i < (lag); i++) { \
             b = sub32b(&obj->x[i], b); \
         } \
         /* Process a special case: L < H \
-           H*2^(kw) + L = (H - 1)*(2^(kw) + 1) + (2^(kw) + 1 + L - H) \
+           H*2^(kw) + L = (H - 1)*(2^(kw) + 2) + (2^(kw) + 2 + L - H) \
            Note that (L - H) mod 2^(kw) gives (L - H + 2^(kw)) */ \
         if (b == 0) { \
             obj->x[(lag)] = 0; \
         } else { \
             obj->c--; \
-            c = 1; \
+            c = 2; \
             for (int i = 0; i < (lag); i++) { \
                 c = add32c(&obj->x[i], c); \
             } \
@@ -158,7 +175,8 @@ static inline uint64_t funcname(type *obj) \
         } \
         obj->pos = 0; \
     } \
-    return obj->x[obj->pos++]; \
+    const uint64_t u = obj->x[obj->pos++]; \
+    return u; \
 }
 
 
@@ -192,19 +210,23 @@ static void *funcname(const GeneratorInfo *gi, const CallerAPI *intf) \
     return obj; \
 }
 
+GENERATE_GET_BITS64_FUNC(get_bits_mwc256u64_raw, MwcFp256u64State, 18145911855674681826U, 4)
+GENERATE_CREATE_BITS64_FUNC(create_mwc256u64, MwcFp256u64State, 4)
+MAKE_GET_BITS_WRAPPERS(mwc256u64)
 
-GENERATE_GET_BITS64_FUNC(get_bits_mwc512u64_raw, MwcFpState, 16166422908038702740U, 8)
+
+GENERATE_GET_BITS64_FUNC(get_bits_mwc512u64_raw, MwcFpState, 13115896780146644418U, 8)
 GENERATE_CREATE_BITS64_FUNC(create_mwc512u64, MwcFpState, 8)
 MAKE_GET_BITS_WRAPPERS(mwc512u64)
 
+GENERATE_GET_BITS64_FUNC(get_bits_mwc1024u64_raw, MwcFp1024u64State, 16279984197873894135U, 16)
+GENERATE_CREATE_BITS64_FUNC(create_mwc1024u64, MwcFp1024u64State, 1)
+MAKE_GET_BITS_WRAPPERS(mwc1024u64)
 
-GENERATE_GET_BITS32_FUNC(get_bits_mwc256u32_raw, MwcFp256u32State, 3634215510U, 8)
+GENERATE_GET_BITS32_FUNC(get_bits_mwc256u32_raw, MwcFp256u32State, 3906776790U, 8)
 GENERATE_CREATE_BITS32_FUNC(create_mwc256u32, MwcFp256u32State, 8)
 MAKE_GET_BITS_WRAPPERS(mwc256u32)
 
-GENERATE_GET_BITS32_FUNC(get_bits_mwc32u32_raw, MwcFp32u32State, 3256128660U, 1)
-GENERATE_CREATE_BITS32_FUNC(create_mwc32u32, MwcFp32u32State, 1)
-MAKE_GET_BITS_WRAPPERS(mwc32u32)
 
 
 
@@ -216,7 +238,7 @@ static inline void *create(const CallerAPI *intf)
 }
 
 
-static int run_self_test(const CallerAPI *intf)
+static int test64(const CallerAPI *intf)
 {
     MwcFpState *obj = intf->malloc(sizeof(MwcFpState));    
     for (int i = 0; i < 8; i++) {
@@ -228,25 +250,61 @@ static int run_self_test(const CallerAPI *intf)
     for (long i = 0; i < 80000; i++) {
         (void) get_bits_mwc512u64_raw(obj);
     }
-
     for (int i = 0; i < 32; i++) {
         const uint64_t u = get_bits_mwc512u64_raw(obj);
         intf->printf("%llX ", (unsigned long long) u);
         if (i % 8 == 7) {
-            intf->printf("| %llX %llX\n", (unsigned long long) obj->c, obj->x[8]);
+            intf->printf("| %llX %llX\n",
+                (unsigned long long) obj->c, (unsigned long long) obj->x[8]);
         }
     }
     intf->printf("\n");
     intf->free(obj);
     return 1;
-    
+}
+
+
+static int test32(const CallerAPI *intf)
+{
+    MwcFp256u32State *obj = intf->malloc(sizeof(MwcFp256u32State));
+    for (int i = 0; i < 8; i++) {
+        obj->x[i] = 0;
+    }
+    obj->x[8] = 1;
+    obj->c = 0;
+    obj->pos = 8;
+    for (long i = 0; i < 80000; i++) {
+        (void) get_bits_mwc256u32_raw(obj);
+    }
+    for (int i = 0; i < 32; i++) {
+        const uint32_t u = (uint32_t) get_bits_mwc256u32_raw(obj);
+        intf->printf("%lX ", (unsigned long) u);
+        if (i % 8 == 7) {
+            intf->printf("| %lX %lX\n",
+                (unsigned long) obj->c, (unsigned long) obj->x[8]);
+        }
+    }
+    intf->printf("\n");
+    intf->free(obj);
+    return 1;
+}
+
+
+
+static int run_self_test(const CallerAPI *intf)
+{
+    const int is32 = test32(intf);
+    const int is64 = test64(intf);
+    return is32 | is64;
 }
 
 static const GeneratorParamVariant gen_list[] = {
     {"",       "mwc512u64", 64, create_mwc512u64, get_bits_mwc512u64, get_sum_mwc512u64},
     {"512u64", "mwc512u64", 64, create_mwc512u64, get_bits_mwc512u64, get_sum_mwc512u64},
+    {"1024u64", "mwc1024u64", 64, create_mwc1024u64, get_bits_mwc1024u64, get_sum_mwc1024u64},
+    {"256u64", "mwc256u64", 64, create_mwc256u64, get_bits_mwc256u64, get_sum_mwc256u64},
     {"256u32", "mwc256u32", 32, create_mwc256u32, get_bits_mwc256u32, get_sum_mwc256u32},
-    {"32u32",  "mwc32u32",  32, create_mwc32u32,  get_bits_mwc32u32,  get_sum_mwc32u32},
+//    {"32u32",  "mwc32u32",  32, create_mwc32u32,  get_bits_mwc32u32,  get_sum_mwc32u32},
     GENERATOR_PARAM_VARIANT_EMPTY
 };
 
---
+++ misc/mwcfp/mwcfp_search.py
@@ -0,0 +1,19 @@
+import sympy, random
+b = 2**2048 + 2
+
+for i in range(10000000):
+    a = random.randint(2**63 + 1, 2**64 - 1)
+    #a = random.randint(2**31 + 1, 2**32 - 1)
+    #a = 16166422908038702740
+    m = a*b - 1
+    if sympy.isprime(m):
+        print(a)
+        #print(hex(b), hex(pow(a, -1, m)))
+        f = sympy.factorint((m - 1) // 2, limit=100_000)
+        if all([sympy.isprime(x) for x in f]):
+            o = sympy.n_order(a, m)
+            if o == m - 1:
+                print("WOW!", a)                
+                break
+            else:
+                print("====>", o, o / m)
