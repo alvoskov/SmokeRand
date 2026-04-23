e640772b6cbefff47b498bd6fce24a1634d067cd
Alexey Voskov <alvoskov@gmail.com>
2026-04-23 20:36:58 +0300

    MWCFP new data

--- generators/mwcfp.c
+++ generators/mwcfp.c
@@ -34,22 +34,22 @@
  *
  * Tests results
  *
- *  Generator  | SmokeRand | TestU01  | PractRand 0.96 | cpb
- * ------------|-----------|----------|----------------|-----
- *  mwc64u32   | full/b64  | +        | >= 16 TiB      | 0.68
- *  mwc128u32  | full      | +        |                | 0.68
- *  mwc256u32  | full      | +        |                | 0.90
- *  mwc512u32  | full      |          |                | 1.3
- *  mwc1024u32 | full      | +        |                | 0.8
- *  mwc2048u32 | full      | +        |                | 1.0
- *  mwc8192u32 | full      | +        | >= 16 TiB      | 0.96
- *  mwc128u64  | full/b64  |          | >= 8 TiB       | 0.46
- *  mwc256u64  | full      |          |                | 0.41
- *  mwc512u64  | full      |          |                | 0.61
- *  mwc1024u64 | full      |          |                | 0.80
- *  mwc2048u64 | full      |          |                | 0.45
- *  mwc4096u64 | full      |          | >= 16 TiB(?)   | 0.55
- *  mwc16384u64| full      |          | >= 16 TiB      | 0.52
+ *  Generator  | SmokeRand | TestU01     | PractRand 0.96 | cpb
+ * ------------|-----------|-------------|----------------|-----
+ *  mwc64u32   | full/b64  | +           | >= 16 TiB      | 0.68
+ *  mwc128u32  | full      | +           | >= 16 TiB      | 0.68
+ *  mwc256u32  | full      | +           |                | 0.90
+ *  mwc512u32  | full      | +           |                | 1.3
+ *  mwc1024u32 | full      | +           |                | 0.8
+ *  mwc2048u32 | full      | +           |                | 1.0
+ *  mwc8192u32 | full      | +           | >= 16 TiB      | 0.96
+ *  mwc128u64  | full/b64  | +IL/+HI/+LO | >= 8 TiB       | 0.46
+ *  mwc256u64  | full      |             |                | 0.41
+ *  mwc512u64  | full      |             |                | 0.61
+ *  mwc1024u64 | full      |             |                | 0.80
+ *  mwc2048u64 | full      |             |                | 0.45
+ *  mwc4096u64 | full      |             | >= 16 TiB(?)   | 0.55
+ *  mwc16384u64| full      | +IL         | >= 16 TiB      | 0.52
  *
  * (c) 2026 Alexey L. Voskov, Lomonosov Moscow State University.
  * alvoskov@gmail.com
